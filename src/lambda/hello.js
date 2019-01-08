e429f2b020bdf0c96cd9c994ec484f6baaa6c843
Andrew Del Prete <pathsofdesign@gmail.com>
2019-01-08 02:23:11 -0600

    wip

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,14 +1,52 @@
+import _ from "lodash";
 require("dotenv").config();
+var Airtable = require("airtable");
 
-// show object spread works, i.e. babel works
-const obj = {
-  foo: "bar"
-};
+var base = new Airtable({ apiKey: "keyO8ujECCZ8EPykr" }).base("appenjpG5GSATPlz0");
+
 export async function handler(event, context) {
-  console.log(process.env.TEST);
-  console.log("queryStringParameters", event.queryStringParameters);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ msg: process.env.TEST, ...obj })
-  };
+  try {
+    let events = await getTable({ maxRecords: 5, table: "events" });
+    const eventsIds = Object.values(events).map(e => e.fields.id);
+
+    const sessions = await getTable({
+      filterByFormula: `OR(${buildFieldIdstring("event_id", eventsIds)})`,
+      table: "sessions"
+    });
+
+    events = Object.values(events).map(e => ({
+      ...e,
+      sessions: sessions[e.fields.sessions[0]]
+    }));
+
+    return { statusCode: 200, body: JSON.stringify(events) };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function buildFieldIdstring(fieldId, recordArr) {
+  return `${recordArr
+    .map(record => `event_id = '${record}',`)
+    .join("")
+    .slice(0, -1)}`;
+}
+
+export async function getTable({ table, ...options }) {
+  let results = [];
+  await base(table)
+    .select(options)
+    .eachPage(function page(records, fetchNextPage) {
+      records.forEach(record => {
+        results = [...results, record._rawJson];
+      });
+
+      fetchNextPage();
+    });
+
+  return results.reduce((result, item, index, array) => {
+    result[item.id] = item;
+    delete result[item.id].id;
+    return result;
+  }, {});
 }
